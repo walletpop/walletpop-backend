2a804901f2c1318fed12fe97f4775ffdf96dd01c
Irene Martin <79365505+IMartinMenendez@users.noreply.github.com>
2023-02-08 11:30:38 +0000

    added unique to the email and changed the location value

--- db/User.js
+++ db/User.js
@@ -5,17 +5,19 @@ const User = sequelize.define('user', {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4,
         allowNull: false,
-        primaryKey: true
+        primaryKey: true,
+        unique: true
       },
     email: {
       type:Sequelize.STRING,
       allowNull: false,
+      unique: true
     },
     password: {
       type: Sequelize.STRING,
       allowNull: false,
     },
-    location: Sequelize.INTEGER,
+    location: Sequelize.STRING,
     isAdmin: {
       type: Sequelize.BOOLEAN,
       defaultValue: false
