d71858eede4a9eb094f845c011364dcc4c9d6d6e
Lorena Gil <67319308+logiflo@users.noreply.github.com>
2023-03-08 11:39:23 +0000

    Update sold endpoints

--- routes/sold.js
+++ routes/sold.js
@@ -2,23 +2,22 @@ const {Router} = require("express");
 const soldRouter = Router();
 const { User, Item, SoldItem } = require("../db");
 const {isAdmin} = require("../middleware/authJwt");
-soldRouter.use(isAdmin);
+
 
 soldRouter.get('/', async (req, res) => {
     try{
-        const soldItems = await SoldItem.findAll({
-            include: [
-                {
-                    model: Item,
-                    include: [User]
-                },
-                {
-                    model: User,
-                    as: "buyer"
+        const user = await User.findByPk(req.cookies.userId)
+        if (user.isAdmin) {
+            const allSoldItems = await SoldItem.findAll();
+            res.status(200).send(allSoldItems);
+        } else {
+            const soldItems = await SoldItem.findAll({
+                where: {
+                    buyerId: user.id
                 }
-            ]
-        });
-        res.status(200).send(soldItems);
+            });
+            res.status(200).send(soldItems);
+        }
     } catch (error){
         return res.status(500).send({ message: error.message });
     }
@@ -26,20 +25,19 @@ soldRouter.get('/', async (req, res) => {
 
 soldRouter.get("/user/:buyer_id", async (req, res) => {
     try{
-        const soldItems = await SoldItem.findAll({
-            where: {buyerId: req.params.buyer_id},
-            include: [
-                {
-                    model: Item,
-                    include: [User]
-                },
-                {
-                    model: User,
-                    as: "buyer"
-                }
-            ]
+        const user = await User.findByPk(req.cookies.userId);
+        const soldItem = await SoldItem.findAll({
+            where: {
+                buyerId: user.id
+            }
         });
-        res.status(200).send(soldItems);
+
+        if (user.isAdmin || user.id == req.params.buyer_id) {
+            res.status(200).send(soldItem);
+        } else {
+            return res.status(401).send({ message: "Unauthorized!" })
+        }
+
     } catch (error){
         return res.status(500).send({ message: error.message });
     }
@@ -47,31 +45,25 @@ soldRouter.get("/user/:buyer_id", async (req, res) => {
 
 soldRouter.post("/", async (req, res) => {
     try{
-        const {dateSold, itemId, buyerId} = req.body;
-        const newSoldItem = await SoldItem.create({dateSold});
-        await newSoldItem.setItem(itemId);
-        await newSoldItem.setBuyer(buyerId);
-        res.status(200).send(newSoldItem);
+        const {itemId, dateSold} = req.body;
+        const item = await Item.findByPk(itemId);
+        let soldItem = null;
+        if(dateSold) {
+            soldItem = await item.createSoldItem({dateSold});
+        }else {
+            soldItem = await item.createSoldItem();
+        }
+        await soldItem.setBuyer(req.cookies.userId);
+        res.status(200).send(soldItem);
     } catch (error){
         return res.status(500).send({ message: error.message });
     }
 });
 
-soldRouter.put("/:id", async (req, res) => {
+soldRouter.delete("/:id", isAdmin, async (req, res) => {
     try{
         const item = await SoldItem.findByPk(req.params.id);
-        const user = await User.findByPk(req.cookies.userId);
-        const editedSoldItem = await item.update(req.body, {where: {id: req.params.id}});
-        res.status(200).send(editedSoldItem);
-    } catch (error){
-        return res.status(500).send({ message: error.message });
-    }
-});
-
-soldRouter.delete("/:id", async (req, res) => {
-    try{
-        const item = await SoldItem.findByPk(req.params.id);
-        const deletedSoldItem = await item.destroy();
+        await item.destroy();
         res.status(200).send("Sold item deleted successfully!");
 
     } catch (error){
